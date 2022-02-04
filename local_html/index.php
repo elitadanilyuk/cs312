7f83be650e531be0f334b22a0a51cff4cf517da5
Elita Danilyuk <elitadanilyuk@gmail.com>
2022-02-04 08:51:02 -0700

    adding the CSU local index page

---
+++ local_html/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="UTF-8">
+        <meta name="author" content="Elita Danilyuk">
+        <meta name="description" content="This is a basing homepage landing, made for and managed by Elita Danilyuk.">
+        <meta name="keywords" content="Homepage, Elita, Danilyuk, CSU, Colorado State University">
+		<title>Elita Danilyuk's Homepage</title>
+	</head>
+    
+    <body>
+        <nav>
+            <a href="#">Home</a> - 
+            <a href="https://www.codecademy.com/learn/learn-html/modules/learn-html-elements/cheatsheet" target="_blank"> HTML Cheat Sheet</a> - 
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank">Learn HTML</a> - 
+            <a href="east/east.php">East</a> - 
+            <a href="west/west.php">West</a>
+        </nav>
+        <header>
+            <h1>Elita Danilyuk's Local Homepage</h1>
+        </header>
+        <main>
+            <img src="photos/elitadanilyuk.jpeg">
+            <p>Welcome to my homepage! Get to know me a bit:</p>
+            <ul>
+                <li>I am a transfer student from Front Range Community College.</li>
+                <li>I am a Junior, majoring in Computer Science with a concentration in Software Engineering.</li>
+                <li>Ukrainian was the first language I learned.</li>
+            </ul>
+        </main>
+        <footer>
+            <p font-size="small">This file should be found through cs.colostate.edu:4444/~elita/</p>
+        </footer>
+    </body>
+</html>
